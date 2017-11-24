30272321af2df896b93dc9a372fda358eefa126d
chrisofarabia <chrisofarabia@yahoo.co.uk>
2017-11-24 15:01:11 +0000

    JS script displays username on page

--- js/script.js
+++ js/script.js
@@ -3,10 +3,12 @@ var userdata = $.ajax({
   url: 'https://randomuser.me/api/',
   dataType: 'json',
   success: function(data) {
-    // console.log(userdata.responseJSON.results[0].name);
+    var honorific = document.getElementById('honorific');
+    honorific.innerHTML = data.results[0].name.title;
+    var firstname = document.getElementById('firstname');
+    firstname.innerHTML = data.results[0].name.first;
+    var lastname = document.getElementById('lastname');
+    lastname.innerHTML = data.results[0].name.last;
+    console.log(data.results[0].name.title + ' ' + data.results[0].name.first + ' ' + data.results[0].name.last);
   }
 });
-
-var lastname = document.getElementById('lastname');
-lastname.innerHTML = userdata.responseJSON.results[0].name.last;
-// console.log(userdata.responseJSON.results[0].name.last);

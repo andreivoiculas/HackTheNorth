f45eb930e2a017eebbadacbff0bd406c92afd2db
chrisofarabia <chrisofarabia@yahoo.co.uk>
2017-11-24 13:50:16 +0000

    Added HTML and other folders

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// console.log('script.js says "I\'m here"');
+var userdata = $.ajax({
+  url: 'https://randomuser.me/api/',
+  dataType: 'json',
+  success: function(data) {
+    // console.log(userdata.responseJSON.results[0].name);
+  }
+});
+
+var lastname = document.getElementById('honorific');
+lastname.innerHTML = userdata.responseJSON.results[0].name.last;
+// console.log(userdata.responseJSON.results[0].name.last);
